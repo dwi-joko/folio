8c04a8ca2c1cac19458ca977b68d28a235913480
Dwi Joko Purnomo <you@example.com>
2022-07-20 23:56:46 +0700

    codding desktop version

---
+++ js/custom.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".slide-service").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    autoplaySpeed: 2000,
+  });
+});
