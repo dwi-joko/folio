057bc0b66797a968d932c9a7230ec3a70f274c0b
Dwi Joko Purnomo <you@example.com>
2022-07-21 15:36:18 +0700

    adjust mobile view

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,24 @@
+$(window).scroll(function () {
+  if ($(window).scrollTop() >= 40) {
+    $(".header").addClass("fixed");
+    // $("header").slideDown(1000);
+  } else {
+    $(".header").removeClass("fixed");
+    // $("header").slideUp(1000);
+  }
+});
 $(document).ready(function () {
   $(".slide-service").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    dots: false,
+    autoplaySpeed: 2000,
+  });
+});
+
+$(document).ready(function () {
+  $(".slide-about").slick({
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows: true,
